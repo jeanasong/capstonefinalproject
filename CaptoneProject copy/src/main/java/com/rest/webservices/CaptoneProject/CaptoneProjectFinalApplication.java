302b782df877930debb1bfa526068963eab5b4d9
jeanasong <58866533+jeanasong@users.noreply.github.com>
2021-03-02 11:43:29 -0500

    FinalProject

---
+++ CaptoneProject copy/src/main/java/com/rest/webservices/CaptoneProject/CaptoneProjectFinalApplication.java
@@ -0,0 +1,12 @@
+package com.rest.webservices.CaptoneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptoneProjectFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptoneProjectFinalApplication.class, args);
+	}
+}
